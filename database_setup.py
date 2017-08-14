7a9a2f88d3189d431a8b2a67d8291edac518b0cc
StefanM <stefan.fva@gmail.com>
2017-08-14 08:54:36 -0400

    Completed full functionallity

--- database_setup.py
+++ database_setup.py
@@ -12,6 +12,12 @@ Base = declarative_base()
 secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits)
 	for x in xrange(32))
 
+def dump_datetime(value):
+    # Deserialize datetime object into string form for JSON processing
+    if value is None:
+        return None
+    return [value.strftime("%Y-%m-%d"), value.strftime("%H:%M:%S")]
+
 
 class User(Base):
 	__tablename__ = 'user'
@@ -24,17 +30,27 @@ class User(Base):
 class Catagory(Base):
 	__tablename__ = 'catagory'
 	id = Column(Integer, primary_key = True)
-	name = Column(String(90))
+	name = Column(String(90), nullable=False)
 	time_created = Column(DateTime(timezone=True), server_default=func.now())
 	time_updated = Column(DateTime(timezone=True), onupdate=func.now())
 	user_id = Column(Integer, ForeignKey('user.id'))
 	user = relationship(User)
 
+	@property
+	def serialize(self):
+    	#Returns object data in easily serializeable format
+    		return {
+				'id' : self.id,
+				'name' : self.name,
+            	'created' : dump_datetime(self.time_created),
+				'updated' : dump_datetime(self.time_updated)
+    		}
+
 
 class Item(Base):
 	__tablename__ = 'item'
 	id = Column(Integer, primary_key = True)
-	name = Column(String(120))
+	name = Column(String(120), nullable=False)
 	description = Column(String(1000))
 	image = Column(String, nullable = False)
 	item_catagory = Column(String, ForeignKey('catagory.name'))
@@ -43,6 +59,19 @@ class Item(Base):
 	time_updated = Column(DateTime(timezone=True), onupdate=func.now())
 	user_id = Column(Integer, ForeignKey('user.id'))
 	user = relationship(User)
+
+	@property
+	def serialize(self):
+        #Returns object data in easily serializeable format
+        	return {
+				'id' : self.id,
+				'name' : self.name,
+	            'description' : self.description,
+				'image' : self.image,
+				'catagory' : self.item_catagory,
+				'created' : dump_datetime(self.time_created),
+				'updated' : dump_datetime(self.time_updated)
+        	}
 
 
 engine = create_engine('sqlite:///catalog.db')
